699cd44e3ff9c84d9c85bdb2251f70710ac8771d
Ye Myat Hein <yemyathein67@gmail.com>
2026-03-30 17:28:45 +0630

    feat: Rate Limiting feature for protectionn

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.ts";
 import { protectRoute } from "../middleware/auth.middleware.ts";
-
+import { arcjetProtection } from "../middleware/arcjet.middleware.ts";
 const router = express.Router();
 
-router.post('/signup', signup)
-router.post('/login', login)
-router.post('/logout', logout)
-router.post('/update-profile', protectRoute, updateProfile)
-router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user))
+router.use(arcjetProtection)
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+router.post('/update-profile', protectRoute, updateProfile);
+router.get("/check", protectRoute, (req:any, res) => res.status(200).json(req.user));
+
 export default router;
