b40a6d5e3fc991b7c26723cb6124af3c40242941
Ye Myat Hein <yemyathein67@gmail.com>
2026-03-31 17:48:39 +0630

    feat: Messages saved to db

--- backend/src/routes/message.route.ts
+++ backend/src/routes/message.route.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import { getAllContacts, getMessageByUserId,sendMessage, getChatPartners } from "../controllers/message.controller.ts";
+import { protectRoute } from "../middleware/auth.middleware.ts";
 
 const router = express.Router();
 
-router.get("/send", (req, res) => {
-    res.send("Send Messages Endpoint")
-})
+
+router.get("/contacts",protectRoute, getAllContacts);
+router.get("/chats", protectRoute, getChatPartners);
+router.get("/:id",protectRoute, getMessageByUserId);
+router.post("/send/:id", protectRoute, sendMessage)
 
 export default router;
