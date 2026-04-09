c61bcd3cdbc9a6760746e84b31a580a8a3042a54
Ye Myat Hein <yemyathein67@gmail.com>
2026-04-09 16:35:28 +0630

    feat: messages container

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import NoChatHistoryPlaceholder from "./NoChatHistoryPlaceholder";
 
-const ChatContainer = () => {
+function ChatContainer() {
+  const {
+    selectedUser,
+    getMessagesByUserId,
+    messages,
+    isMessagesLoading,
+  } = useChatStore();
+  const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
+
+  useEffect(() => {
+    getMessagesByUserId(selectedUser._id);
+  }, [selectedUser, getMessagesByUserId]);
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
+
   return (
-    <div>ChatContainer</div>
-  )
+    <>
+      <ChatHeader />
+      <div className="flex-1 px-6 overflow-y-auto py-8">
+          {messages.length  > 0 ? (
+            <div className="max-w-3xl mx-auto space-y-6">
+              {
+                messages.map(msg  => (
+                  <div key={msg._id} className={`chat ${msg.sederId === authUser._id ? "chat-end" : "chat-start"}`}>
+                    <div className={`chat-bubble relative ${msg.sederId === authUser._id ? 'bg-cyan-600 text-white' : "bg-slate-800 text-slate-200"}`}>
+                      {msg.image && (
+                        <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover"/>
+                      )}
+                      {msg.text && <p className="mt-2">{msg.text}</p>}
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+          ) : (
+            <NoChatHistoryPlaceholder name={selectedUser.fullName} />
+          )}
+      </div>
+    </>
+  );
 }
 
-export default ChatContainer
+export default ChatContainer;
--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -45,4 +45,16 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
+  getMessagesByUserId : async (userId) => {
+    set({isMessagesLoading:true})
+    try {
+      const res = await axiosInstance.get(`/messages/${userId}`)
+      set({messages: res.data})
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong")
+    } finally{
+      set({isMessagesLoading: false})
+    }
+  }
+
 }));
