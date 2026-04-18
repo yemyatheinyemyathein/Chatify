db0eb3430d177ac0c99c2c03314154955b3b32b6
Ye Myat Hein <yemyathein67@gmail.com>
2026-04-19 01:19:23 +0630

    feat: send Message Ready

--- frontend/src/components/ChatContainer.tsx
+++ frontend/src/components/ChatContainer.tsx
@@ -34,25 +34,31 @@ function ChatContainer() {
             <div className="max-w-3xl mx-auto space-y-6">
               {
                 messages.map(msg  => (
-                  <div key={msg._id} className={`chat ${msg.sederId === authUser._id ? "chat-end" : "chat-start"}`}>
-                    <div className={`chat-bubble relative ${msg.sederId === authUser._id ? 'bg-cyan-600 text-white' : "bg-slate-800 text-slate-200"}`}>
+                  <div key={msg._id} className={`chat ${msg.senderId === authUser._id ? "chat-end" : "chat-start"}`}>
+                    <div className={`chat-bubble relative ${msg.senderId === authUser._id ? 'bg-cyan-600 text-white' : "bg-slate-800 text-slate-200"}`}>
                       {msg.image && (
                         <img src={msg.image} alt="Shared" className="rounded-lg h-48 object-cover"/>
                       )}
                       {msg.text && <p className="mt-2">{msg.text}</p>}
                       <p className="text-xs mt-1 opacity-75 flex items-center gap-1">
-                        {new Date(msg.createdAt).toLocaleTimeString([], {hour: "2-digit", minute: "2-digit"})}
+                        {
+                          new Date(msg.createdAt).toLocaleTimeString(undefined, {
+                            hour: "2-digit",
+                            minute: "2-digit"
+                          })
+                        }
                       </p>
                     </div>
                   </div>
                 ))
               }
+              <div ref={messageEndRef}/>
             </div>
           ) : isMessagesLoading? <MessagesLoadingSkeleton /> : (
             <NoChatHistoryPlaceholder name={selectedUser.fullName} />
           )}
       </div>
-      <MessageInput />ၐ
+      <MessageInput />
     </>
   );
 }
--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -45,26 +45,42 @@ export const useChatStore = create((set, get) => ({
     }
   },
 
-  getMessagesByUserId : async (userId) => {
-    set({isMessagesLoading:true})
+  getMessagesByUserId: async (userId) => {
+    set({ isMessagesLoading: true });
     try {
-      const res = await axiosInstance.get(`/messages/${userId}`)
-      set({messages: res.data})
+      const res = await axiosInstance.get(`/messages/${userId}`);
+      set({ messages: res.data });
     } catch (error) {
-      toast.error(error.response?.data?.message || "Something went wrong")
-    } finally{
-      set({isMessagesLoading: false})
+      toast.error(error.response?.data?.message || "Something went wrong");
+    } finally {
+      set({ isMessagesLoading: false });
     }
   },
 
   sendMessage: async (messageData) => {
-    const {selectedUser, messages} = get();
+    const { selectedUser, messages } = get();
+    const {authUser} = useAuthStore.getState();
+    const tempId = `tempId-${Date.now()}`
+    const optimisticMessage = {
+      _id: tempId, 
+      senderId: authUser._id,
+      receiverId: selectedUser._id,
+      text: messageData.text,
+      image: messageData.image,
+      createdAt: new Date().toISOString(),
+      isOptimistic: true, 
+    };
+    set({messages: [...messages, optimisticMessage]})
     try {
-      const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData)
-      set({messages : messages.cocat(res.data)})
+      const res = await axiosInstance.post(
+        `/messages/send/${selectedUser._id}`,
+        messageData
+      );
+      set({ messages: messages.concat(res.data) });
     } catch (error) {
-      toast.error(error.response?.data?.message || "Something went wrong")
+      set({messages: messages})
+      console.log("Console Error", error);
+      toast.error(error.response?.data?.message || "Something went wrong");
     }
-  }
-
+  },
 }));
