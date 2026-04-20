56b29bb04f4be4d3d73b612f58aebeec84a8e79c
Ye Myat Hein <yemyathein67@gmail.com>
2026-04-20 12:49:35 +0630

    feat: update package.json

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,4 +3,4 @@ import axios from "axios";
 export const axiosInstance = axios.create({
     baseURL: import.meta.env.MODE === 'development' ? "http://localhost:3000/api" : "/api",
     withCredentials: true,
-})
+});
